58b46199f537c1fac604f78bf7e27dcab6c40045
chiagozie pio <nduagubapio@gmail.com>
2023-09-05 21:19:18 +0100

    finished the project

--- form.js
+++ form.js
@@ -16,21 +16,61 @@ function validateFullname(){
         nameError.textContent = "required"
         return false;
     }
-    if(!forName.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+    else if(!forName.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
         nameError.textContent = "write fullname";
         return false;
     }
     nameError.innerHTML ='<i class="fa-solid fa-circle-check"></i>'
     return true;
-}
+};
 function validateTel(){
     let forTel = tel.value.trim();
     if(forTel.length == 0){
         telError.textContent = "required";
         return false;
     }
-    if(forTel.length !== 11){
-        telError.textContent = "must be up to 10 digits"
+    else if(forTel.length !== 11){
+        telError.textContent = "must be up to 11 digits"
         return false;
     }
-}
+    else if(!forTel.match(/^[0-9]{11}$/)){
+        telError.textContent = "numbers only";
+        return false;
+    }
+    telError.innerHTML ='<i class="fa-solid fa-circle-check"></i>'
+    return true;
+};
+
+function validateMail(){
+    let forEmail = email.value.trim();
+    if(forEmail.length == 0){
+        emailError.textContent = "required";
+        return false
+    }else{
+        emailError.innerHTML ='<i class="fa-solid fa-circle-check"></i>'
+        return true;
+    }
+    
+};
+
+function validateText(){
+    let forText = text.value.trim();
+    let limit = 30;
+    let remaining =limit - forText.length;
+    if(remaining > 0){
+        textError.textContent = remaining + "characters remaining";
+        return false;
+    }
+    textError.innerHTML ='<i class="fa-solid fa-circle-check"></i>'
+        return true;
+};
+
+/* match(/^[A-Za-z]\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/) */
+
+function validadteAll(){
+    if(!validateFullname() || !validateMail() || !validateTel() || !validateText()  ){
+        formError.textContent = "fill the blanks";
+        return false;
+    }
+    
+};
